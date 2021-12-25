e59c58beebbc05ba9041c0381c8a19c70731b0a4
ak465 <76836592+ak465@users.noreply.github.com>
2021-12-25 16:38:36 +0530

    Update 110op.py

--- 110op.py
+++ 110op.py
@@ -1,26 +1,41 @@
 import csv
-import statistics
 import pandas as pd
-import random
 import plotly.figure_factory as ff
+import statistics
+import plotly.graph_objects as go
+import random
 
-df = pd.read_csv("medium_data.csv")
-data = df["reading_time"].tolist()
-fig = ff.create_distplot([data], ["reading_time"], show_hist= False)
-fig.show()
+df = pd.read_csv('medium_data.csv')
+data = df['reading_time'].tolist()
+population_mean=statistics.mean(data)
+Sampling=statistics.mean(data)
+print("population mean is : ",population_mean)
+print("Sampling mean: ",Sampling)
 
-population_mean = statistics.mean(data)
-print("Mean = ", population_mean)
-population_standarddeviation = statistics.stdev(data)
-print("Standard Deviation = ", population_standarddeviation)
 
-#Code to find mean and standard deviation of 100 data points 
-dataset = []
-for i in range (0,1000):
-    random_index = random.randint(0, len(data))
-    value = data[random_index]
-    dataset.append(value)
-mean = statistics.mean(dataset)
-standarddeviation = statistics.stdev(dataset)
-print("Mean of 1000 values ", mean)
-print("Standard deviation of 1000 values", standarddeviation)
+def random_set_means(counter):
+    dataset=[]
+    for i in range(0,counter):
+        random_index=random.randint(0,len(data)-1)
+        value=data[random_index]
+        dataset.append(value)
+    mean=statistics.mean(dataset)
+    return mean
+
+def show_fig(mean_list):
+    df=mean_list
+    fig = ff.create_distplot([df], ['temp'], show_hist=False)
+    fig.show()
+
+def setup():
+    mean_list=[]
+    for i in range(0,100):
+        set_of_means=random_set_means(30)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+
+setup()
+
+
+
+
